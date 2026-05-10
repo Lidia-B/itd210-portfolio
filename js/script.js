c5b4a0df53368f84f191d66d41a51c9341b199b6
Lidia Bernal <ljb67917@email.vccs.edu>
2026-05-10 09:00:59 -0400

    Dark Mode added to pages

--- js/script.js
+++ js/script.js
@@ -214,3 +214,31 @@ document.addEventListener("keydown", (e) => {
 });
 
 });
+
+// =========================
+// DARK MODE TOGGLE
+// =========================
+
+const darkToggle = document.getElementById("darkToggle");
+
+if (darkToggle) {
+  darkToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // save preference
+    if (document.body.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  });
+
+  // load saved theme
+  window.addEventListener("load", () => {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+      document.body.classList.add("dark-mode");
+    }
+  });
+}
